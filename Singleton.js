3b37feee6863cc56a8e1f3b67ec42b63c7d915a9
Sameer Mishra <smishra1000@rediffmail.com>
2021-08-10 09:47:01 +0530

    Update Singleton.js

--- Singleton.js
+++ Singleton.js
@@ -1,4 +1,8 @@
 //Singleton designe pattern
+//The Singleton design pattern is a creational pattern that states that one and only one instance of a 
+//class would persist in the memory during the application’s life cycle. In other words, this design pattern restricts instantiation of a 
+//class to just one object. As far as JavaScript is concerned, 
+//a singleton is an instance that is created just once — any repeated calls to the constructor would always fetch the same instance
 
 var Singleton = (function(){
     // Constructor function based on requirement
