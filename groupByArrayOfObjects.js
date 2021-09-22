7bcd19fb0ae179ec354975ee5c86035d9b1cd895
Sameer Mishra <smishra1000@rediffmail.com>
2021-09-22 11:52:40 +0530

    Update groupByArrayOfObjects.js

--- groupByArrayOfObjects.js
+++ groupByArrayOfObjects.js
@@ -16,3 +16,25 @@ groupedObj[key].push(obj)
 }
 
 console.log("after grouped",groupedObj)
+
+
+======================Using reduce ===============
+	
+	var people = [
+  { name: 'sameer', age: 21 },
+  { name: 'shekhar', age: 20 },
+  { name: 'ravi', age: 20 }
+];
+let groupByProperty = 'age';
+let groupedObj = {};
+
+let data= people.reduce(function(acc,obj){
+let key = obj[groupByProperty]
+if(!acc[key]) {
+	acc[key] = []
+}
+acc[key].push(obj)
+return acc;
+},{})
+
+console.log("after grouped",data)
